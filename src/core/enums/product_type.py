a1bd47368d07d62cf75b93b42d60416e30f868bf
KirMilo <kerill.milovanov2@mail.ru>
2025-11-23 12:22:26 +0300

    Зарефакторил enums

---
+++ src/core/enums/product_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class ProductTypeEnum(Enum):
+    CONSUMABLE = "consumable"
+    PERMANENT = "permanent"
--- src/core/enums/transaction_status.py
+++ src/core/enums/transaction_status.py
@@ -1,11 +1,6 @@
 from enum import Enum
 
 
-class ProductTypeEnum(Enum):
-    CONSUMABLE = "consumable"
-    PERMANENT = "permanent"
-
-
 class TransactionStatusEnum(Enum):
     PENDING = "pending"
     COMPLETED = "completed"
